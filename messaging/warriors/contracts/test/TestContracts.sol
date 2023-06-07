e977f4f57038ab9b53d196e57549e5033fef10f9
Denis Fadeev <denis@fadeev.org>
2023-06-07 13:34:16 +0400

    WIP CCM tests

---
+++ messaging/warriors/contracts/test/TestContracts.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "toolkit/contracts/ZetaEthMock.sol";
+import "@zetachain/protocol-contracts/contracts/evm/tools/ZetaTokenConsumerUniV2.strategy.sol";
