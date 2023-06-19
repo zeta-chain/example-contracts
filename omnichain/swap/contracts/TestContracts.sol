2a522cb8db9057f7bc6a3eae3c50dd41e86dadc1
Denis Fadeev <denis@fadeev.org>
2023-06-19 13:31:11 +0400

    fix: swap test

--- omnichain/swap/contracts/TestContracts.sol
+++ omnichain/swap/contracts/TestContracts.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-import "@zetachain/toolkit/contracts/TestZRC20.sol";
+import "@zetachain/toolkit/contracts/BytesHelperLib.sol";
 import "@zetachain/toolkit/contracts/TestSystemContract.sol";
+import "@zetachain/toolkit/contracts/TestZRC20.sol";
