ea43736eac40f45f011546123b9c4088e336d0dd
Denis Fadeev <denis@fadeev.org>
2023-06-23 18:24:13 +0400

    refactor: remove console log

--- messaging/message/tasks/deploy.ts
+++ messaging/message/tasks/deploy.ts
@@ -48,10 +48,6 @@ const deployContract = async (
     "zetaTokenConsumerUniV3",
     networkName as any
   );
-  console.log(networkName);
-  console.log(`zetaTokenConsumerUniV2: ${zetaTokenConsumerUniV2}`);
-  console.log(`zetaTokenConsumerUniV3: ${zetaTokenConsumerUniV3}`);
-  console.log(zetaTokenConsumerUniV3 || zetaTokenConsumerUniV2);
   const { abi, bytecode } = await hre.artifacts.readArtifact(contractName);
   const factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const contract = await factory.deploy(
