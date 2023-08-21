663b54ccd05fc65c70792921f35fee1086f553c0
Denis Fadeev <denis@fadeev.org>
2023-08-21 11:25:28 +0400

    npx hardhat messaging CrossChainMessage message

---
+++ messaging/message/contracts/CrossChainMessage.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@zetachain/protocol-contracts/contracts/evm/tools/ZetaInteractor.sol";
+import "@zetachain/protocol-contracts/contracts/evm/interfaces/ZetaInterfaces.sol";
+
+interface CrossChainMessageErrors {
+    error InvalidMessageType();
+}
+
+contract CrossChainMessage is
+    ZetaInteractor,
+    ZetaReceiver,
+    CrossChainMessageErrors
+{
+    bytes32 public constant CROSS_CHAIN_MESSAGE_MESSAGE_TYPE =
+        keccak256("CROSS_CHAIN_CROSS_CHAIN_MESSAGE");
+
+    event CrossChainMessageEvent(bytes32);
+    event CrossChainMessageRevertedEvent(bytes32);
+
+    ZetaTokenConsumer private immutable _zetaConsumer;
+    IERC20 internal immutable _zetaToken;
+
+    constructor(
+        address connectorAddress,
+        address zetaTokenAddress,
+        address zetaConsumerAddress
+    ) ZetaInteractor(connectorAddress) {
+        _zetaToken = IERC20(zetaTokenAddress);
+        _zetaConsumer = ZetaTokenConsumer(zetaConsumerAddress);
+    }
+
+    function sendMessage(uint256 destinationChainId, bytes32 message) external payable {
+        if (!_isValidChainId(destinationChainId))
+            revert InvalidDestinationChainId();
+
+        uint256 crossChainGas = 2 * (10 ** 18);
+        uint256 zetaValueAndGas = _zetaConsumer.getZetaFromEth{
+            value: msg.value
+        }(address(this), crossChainGas);
+        _zetaToken.approve(address(connector), zetaValueAndGas);
+
+        connector.send(
+            ZetaInterfaces.SendInput({
+                destinationChainId: destinationChainId,
+                destinationAddress: interactorsByChainId[destinationChainId],
+                destinationGasLimit: 300000,
+                message: abi.encode(CROSS_CHAIN_MESSAGE_MESSAGE_TYPE, message),
+                zetaValueAndGas: zetaValueAndGas,
+                zetaParams: abi.encode("")
+            })
+        );
+    }
+
+    function onZetaMessage(
+        ZetaInterfaces.ZetaMessage calldata zetaMessage
+    ) external override isValidMessageCall(zetaMessage) {
+        (bytes32 messageType, bytes32 message) = abi.decode(
+            zetaMessage.message, (bytes32, bytes32)
+        );
+
+        if (messageType != CROSS_CHAIN_MESSAGE_MESSAGE_TYPE)
+            revert InvalidMessageType();
+
+        emit CrossChainMessageEvent(message);
+    }
+
+    function onZetaRevert(
+        ZetaInterfaces.ZetaRevert calldata zetaRevert
+    ) external override isValidRevertCall(zetaRevert) {
+        (bytes32 messageType, bytes32 message) = abi.decode(
+            zetaRevert.message,
+            (bytes32, bytes32)
+        );
+
+        if (messageType != CROSS_CHAIN_MESSAGE_MESSAGE_TYPE)
+            revert InvalidMessageType();
+
+        emit CrossChainMessageRevertedEvent(message);
+    }
+}
---
+++ messaging/message/tasks/interact.ts
@@ -0,0 +1,37 @@
+import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { parseEther } from "@ethersproject/units";
+import { trackCCTX } from "@zetachain/toolkit/helpers";
+
+const contractName = "CrossChainMessage";
+
+const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
+  const [signer] = await hre.ethers.getSigners();
+  console.log(`🔑 Using account: ${signer.address}\n`);
+
+  const factory = await hre.ethers.getContractFactory(contractName);
+  const contract = factory.attach(args.contract);
+
+  const destination = hre.config.networks[args.destination]?.chainId;
+  if (destination === undefined) {
+    throw new Error(`${args.destination} is not a valid destination chain`);
+  }
+
+  const paramMessage = hre.ethers.utils.toUtf8Bytes(args.message);
+
+  const tx = await contract
+    .connect(signer)
+    .sendMessage(destination,  paramMessage,  { value: parseEther(args.amount) });
+
+  const receipt = await tx.wait();
+  console.log(`✅ The transaction has been broadcasted to ${hre.network.name}
+📝 Transaction hash: ${receipt.transactionHash}
+`);
+  await trackCCTX(tx.hash);
+};
+
+task("interact", "Sends a message from one chain to another.", main)
+  .addParam("contract", "Contract address")
+  .addParam("amount", "Token amount to send")
+  .addParam("destination", "Destination chain")
+  .addParam("message", "bytes32")
