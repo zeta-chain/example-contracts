74d7fd5f7eb001ff5be0182c1f63ad1e0dc9eb13
Denis Fadeev <denis@fadeev.org>
2023-08-25 15:09:31 +0400

    git clone template

--- messaging/warriors/hardhat.config.ts
+++ messaging/warriors/hardhat.config.ts
@@ -1,17 +1,14 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import { getHardhatConfigNetworks } from "@zetachain/networks";
 import "@zetachain/toolkit/tasks";
 
-import "./tasks/deploy";
-import "./tasks/mint";
-import "./tasks/transfer";
+import { getHardhatConfigNetworks } from "@zetachain/networks";
+import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.7",
   networks: {
     ...getHardhatConfigNetworks(),
   },
+  solidity: "0.8.7",
 };
 
 export default config;
