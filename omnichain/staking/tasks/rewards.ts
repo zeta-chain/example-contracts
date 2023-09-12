62562dc3125e6e1ea3711e95789a548c7654bb7f
Denis Fadeev <denis@fadeev.org>
2023-09-12 11:43:48 +0800

    fix tasks

--- omnichain/staking/tasks/rewards.ts
+++ omnichain/staking/tasks/rewards.ts
@@ -1,14 +1,17 @@
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { convertToHexAddress } from "../lib/convertToHexAddress";
 
 const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
   const [signer] = await hre.ethers.getSigners();
   console.log(`🔑 Using account: ${signer.address}\n`);
 
+  const staker = convertToHexAddress(args.staker);
+
   const factory = await hre.ethers.getContractFactory("Staking");
   const contract = factory.attach(args.contract);
 
-  console.log(await contract.queryRewards(args.staker));
+  console.log(await contract.queryRewards(staker));
 };
 
 task("rewards", "Query staking rewards", main)
