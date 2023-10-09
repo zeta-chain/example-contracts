27092595393a9ea74b97c02733f6b3ced05c461e
Denis Fadeev <denis@fadeev.org>
2023-10-09 20:07:50 +0400

    fix integration

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -151,8 +151,7 @@ echo "Testing omnichain Multioutput"
 OMNI_MULTIOUTPUT_CONTRACT=$(npx hardhat deploy --network zeta_testnet --json | jq -r '.address')
 echo $OMNI_MULTIOUTPUT_CONTRACT
 
-npx hardhat destination --contract $OMNI_MULTIOUTPUT_CONTRACT --network goerli_testnet --destination mumbai_testnet
-npx hardhat destination --contract $OMNI_MULTIOUTPUT_CONTRACT --network goerli_testnet --destination bsc_testnet
+npx hardhat destination --contract $OMNI_MULTIOUTPUT_CONTRACT --network zeta_testnet --destination mumbai_testnet
 OMNI_MULTIOUTPUT_TX=$(npx hardhat interact --contract $OMNI_MULTIOUTPUT_CONTRACT --network goerli_testnet --amount 0.01 --recipient $SENDER --json | jq -r '.hash')
 echo $OMNI_MULTIOUTPUT_TX
 OMNI_MULTIOUTPUT_CCTX=$(npx hardhat cctx $OMNI_MULTIOUTPUT_TX --json)
