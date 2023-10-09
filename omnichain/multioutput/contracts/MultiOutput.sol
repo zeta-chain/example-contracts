1242844030263f4cf56f8593074130440fce5aa2
Denis Fadeev <denis@fadeev.org>
2023-10-09 12:06:58 +0400

    npx hardhat omnichain MultiOutput recipient:address

---
+++ omnichain/multioutput/contracts/MultiOutput.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "@zetachain/protocol-contracts/contracts/zevm/SystemContract.sol";
+import "@zetachain/protocol-contracts/contracts/zevm/interfaces/zContract.sol";
+
+contract MultiOutput is zContract {
+    SystemContract public immutable systemContract;
+
+    constructor(address systemContractAddress) {
+        systemContract = SystemContract(systemContractAddress);
+    }
+
+    modifier onlySystem() {
+        require(
+            msg.sender == address(systemContract),
+            "Only system contract can call this function"
+        );
+        _;
+    }
+
+    function onCrossChainCall(
+        zContext calldata context,
+        address zrc20,
+        uint256 amount,
+        bytes calldata message
+    ) external virtual override onlySystem {
+        (address recipient) = abi.decode(
+            message,
+            (address)
+        );
+        // TODO: implement the logic
+    }
+}
