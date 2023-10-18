8a3c8bfbe1cf56bc2ef535ba99d7a946a5080127
Denis Fadeev <denis@fadeev.org>
2023-10-18 12:10:51 +0400

    custom code

--- omnichain/swap/contracts/Swap.sol
+++ omnichain/swap/contracts/Swap.sol
@@ -3,9 +3,14 @@ pragma solidity 0.8.7;
 
 import "@zetachain/protocol-contracts/contracts/zevm/SystemContract.sol";
 import "@zetachain/protocol-contracts/contracts/zevm/interfaces/zContract.sol";
+import "@zetachain/toolkit/contracts/SwapHelperLib.sol";
+import "@zetachain/toolkit/contracts/BytesHelperLib.sol";
 
 contract Swap is zContract {
     SystemContract public immutable systemContract;
+    uint256 constant BITCOIN = 18332;
+    error WrongGasContract();
+    error NotEnoughToPayGasFee();
 
     constructor(address systemContractAddress) {
         systemContract = SystemContract(systemContractAddress);
@@ -25,10 +30,41 @@ contract Swap is zContract {
         uint256 amount,
         bytes calldata message
     ) external virtual override onlySystem {
-        (address targetToken, address recipient) = abi.decode(
-            message,
-            (address, address)
+        address targetTokenAddress;
+        address recipientAddress;
+
+        if (context.chainID == BITCOIN) {
+            targetTokenAddress = BytesHelperLib.bytesToAddress(message, 0);
+            recipientAddress = BytesHelperLib.bytesToAddress(message, 20);
+        } else {
+            (address targetToken, address recipient) = abi.decode(
+                message,
+                (address, address)
+            );
+            targetTokenAddress = targetToken;
+            recipientAddress = recipient;
+        }
+
+        uint256 outputAmount = SwapHelperLib._doSwap(
+            systemContract.wZetaContractAddress(),
+            systemContract.uniswapv2FactoryAddress(),
+            systemContract.uniswapv2Router02Address(),
+            zrc20,
+            amount,
+            targetTokenAddress,
+            0
         );
-        // TODO: implement the logic
+
+        (address gasZRC20, uint256 gasFee) = IZRC20(targetTokenAddress)
+            .withdrawGasFee();
+
+        if (gasZRC20 != targetTokenAddress) revert WrongGasContract();
+        if (gasFee >= outputAmount) revert NotEnoughToPayGasFee();
+
+        IZRC20(targetTokenAddress).approve(targetTokenAddress, gasFee);
+        IZRC20(targetTokenAddress).withdraw(
+            abi.encodePacked(recipientAddress),
+            outputAmount - gasFee
+        );
     }
 }
