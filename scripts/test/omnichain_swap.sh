825b9116bcb61d13327997c434ec95f3be9b1488
Denis Fadeev <denis@fadeev.org>
2023-10-31 12:42:41 +0400

    test

--- scripts/test/omnichain_swap.sh
+++ scripts/test/omnichain_swap.sh
@@ -2,25 +2,25 @@
 
 set -e
 
-echo "testing swap"
+# echo "testing swap"
 
-source "$(dirname "$0")/../common.sh"
+# source "$(dirname "$0")/../common.sh"
 
-echo $SENDER
+SENDER=0x2cD3D070aE1BD365909dD859d29F387AA96911e1
 
-# pushd "./omnichain/swap"
+pushd "./omnichain/swap"
 
-# yarn
+yarn
 
-# npx hardhat compile --force
+npx hardhat compile --force
 
-# echo "Testing omnichain Swap"
+echo "Testing omnichain Swap"
 
-# OMNI_SWAP_CONTRACT=$(npx hardhat deploy --network zeta_testnet --json | jq -r '.address')
-# # echo "omnichain.swap.zeta_testnet=$OMNI_SWAP_CONTRACT" >> contracts.toml
-# OMNI_SWAP_TX=$(npx hardhat interact --contract $OMNI_SWAP_CONTRACT --network goerli_testnet --amount 0.01 --recipient $SENDER --destination mumbai_testnet --json | jq -r '.hash')
-# echo $OMNI_SWAP_TX
-# OMNI_SWAP_CCTX=$(npx hardhat cctx $OMNI_SWAP_TX --json)
-# echo $OMNI_SWAP_CCTX
+OMNI_SWAP_CONTRACT=$(npx hardhat deploy --network zeta_testnet --json | jq -r '.address')
+# echo "omnichain.swap.zeta_testnet=$OMNI_SWAP_CONTRACT" >> contracts.toml
+OMNI_SWAP_TX=$(npx hardhat interact --contract $OMNI_SWAP_CONTRACT --network goerli_testnet --amount 0.01 --recipient $SENDER --destination mumbai_testnet --json | jq -r '.hash')
+echo $OMNI_SWAP_TX
+OMNI_SWAP_CCTX=$(npx hardhat cctx $OMNI_SWAP_TX --json)
+echo $OMNI_SWAP_CCTX
 
-# popd
+popd
