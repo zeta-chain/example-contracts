9f8fa46673b85619726db1468967442f069eb3dc
Denis Fadeev <denis@fadeev.org>
2023-12-19 13:30:49 +0400

    interact

--- omnichain/swap/tasks/interact.ts
+++ omnichain/swap/tasks/interact.ts
@@ -1,10 +1,12 @@
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { parseEther } from "@ethersproject/units";
+import { parseEther, parseUnits } from "@ethersproject/units";
 import { getAddress } from "@zetachain/protocol-contracts";
+import ERC20Custody from "@zetachain/protocol-contracts/abi/evm/ERC20Custody.sol/ERC20Custody.json";
 import { prepareData } from "@zetachain/toolkit/helpers";
 import bech32 from "bech32";
-import { utils } from "ethers";
+import { utils, ethers } from "ethers";
+import ERC20 from "@openzeppelin/contracts/build/contracts/ERC20.json";
 
 const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
   const [signer] = await hre.ethers.getSigners();
@@ -26,10 +28,29 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
     ["address", "bytes"],
     [args.targetToken, recipient]
   );
-  const to = getAddress("tss", hre.network.name);
-  const value = parseEther(args.amount);
 
-  const tx = await signer.sendTransaction({ data, to, value });
+  let tx;
+
+  if (args.token) {
+    const custodyAddress = getAddress("erc20Custody", hre.network.name as any);
+    const custodyContract = new ethers.Contract(
+      custodyAddress,
+      ERC20Custody.abi,
+      signer
+    );
+    const tokenContract = new ethers.Contract(args.token, ERC20.abi, signer);
+    const decimals = await tokenContract.decimals();
+    const value = parseUnits(args.amount, decimals);
+    const approve = await tokenContract.approve(custodyAddress, value);
+    await approve.wait();
+
+    tx = await custodyContract.deposit(signer.address, args.token, value, data);
+    tx.wait();
+  } else {
+    const value = parseUnits(args.amount, 18);
+    const to = getAddress("tss", hre.network.name as any);
+    tx = await signer.sendTransaction({ data, to, value });
+  }
 
   if (args.json) {
     console.log(JSON.stringify(tx, null, 2));
@@ -37,7 +58,7 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
     console.log(`🔑 Using account: ${signer.address}\n`);
 
     console.log(`🚀 Successfully broadcasted a token transfer transaction on ${hre.network.name} network.
-  📝 Transaction hash: ${tx.hash}
+📝 Transaction hash: ${tx.hash}
   `);
   }
 };
@@ -47,4 +68,5 @@ task("interact", "Interact with the contract", main)
   .addParam("amount", "Amount of tokens to send")
   .addFlag("json", "Output in JSON")
   .addParam("targetToken")
+  .addOptionalParam("token", "The address of the token to send")
   .addParam("recipient");
