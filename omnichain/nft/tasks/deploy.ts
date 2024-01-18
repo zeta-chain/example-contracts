8a8ddc973ac90e18ef1121eb873852023b95f884
Denis Fadeev <denis@fadeev.org>
2024-01-18 18:41:43 +0400

    deploy script contract name

--- omnichain/nft/tasks/deploy.ts
+++ omnichain/nft/tasks/deploy.ts
@@ -18,7 +18,7 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
 
   const systemContract = getAddress("systemContract", "zeta_testnet");
 
-  const factory = await hre.ethers.getContractFactory("MyContract");
+  const factory = await hre.ethers.getContractFactory("NFT");
   const contract = await factory.deploy(systemContract);
   await contract.deployed();
 
@@ -34,7 +34,4 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
   }
 };
 
-task("deploy", "Deploy the contract", main).addFlag(
-  "json",
-  "Output in JSON"
-);
+task("deploy", "Deploy the contract", main).addFlag("json", "Output in JSON");
