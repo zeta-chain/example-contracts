ea886e07ed619d19c8d4dd2f0d922bea1dc7ba64
lukema95 <867273263@qq.com>
2024-01-24 21:46:16 +0800

    remove destination cli

--- omnichain/multioutput/tasks/destination.ts
+++ omnichain/multioutput/tasks/destination.ts
@@ -1,23 +1,9 @@
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { getAddress } from "@zetachain/protocol-contracts";
 
 const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
-  const [signer] = await hre.ethers.getSigners();
-  console.log(`🔑 Using account: ${signer.address}\n`);
-
-  const destinationTokens = args.destination.split(",");
-  const ZetaMultiOutput = await hre.ethers.getContractAt(
-    "MultiOutput",
-    args.contract
-  );
-
-  const tx = await ZetaMultiOutput.registerDestinationToken(destinationTokens);
-
-  await tx.wait();
-
   console.log(
-    `Registered token ${destinationTokens} as destination tokens in the contract ${args.contract}`
+    `This feature is no longer used, please check for more information: https://github.com/zeta-chain/example-contracts/issues/101`
   );
 };
 
