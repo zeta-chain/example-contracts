a32d4b3e8f1b3819ca400683c93c496132b28faf
Denis Fadeev <denis@fadeev.org>
2024-04-30 10:50:25 +0300

    git clone template

---
+++ messaging/token/hardhat.config.ts
@@ -0,0 +1,14 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "@zetachain/toolkit/tasks";
+
+import { getHardhatConfigNetworks } from "@zetachain/networks";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  networks: {
+    ...getHardhatConfigNetworks(),
+  },
+  solidity: "0.8.7",
+};
+
+export default config;
