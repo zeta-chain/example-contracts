ffe2083bc96d85d2e4487c4460366453b1afef18
Denis Fadeev <denis@fadeev.org>
2024-06-09 17:14:05 +0900

    nft

---
+++ messaging/message/contracts/NFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    uint256 private _currentTokenId = 0;
+
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    function mintTo(address recipient) public onlyOwner returns (uint256) {
+        uint256 newTokenId = _currentTokenId;
+        _currentTokenId += 1;
+        _mint(recipient, newTokenId);
+        return newTokenId;
+    }
+}
