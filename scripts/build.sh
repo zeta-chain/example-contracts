ef00a92ce7f0719c81048c93af4e5bf0c9ec7cab
Denis Fadeev <denis@fadeev.org>
2024-09-12 18:29:20 +0800

    fix slither

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf abi && mkdir -p abi/examples
+rm -rf typescript-types && mkdir typescript-types
+
+for dir in ./examples/*/; do
+  subdir=$(echo $dir | cut -d'/' -f2)
+  
+  cd $dir && yarn && npx hardhat compile --force && cp -r artifacts/contracts/* ../../abi/examples/ && cd ../../;           
+done
+
+find ./abi/ -name '*.dbg.json' -exec rm {} \;
