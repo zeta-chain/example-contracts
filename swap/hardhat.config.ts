6763ecfca9c1219f8243790475ca7cb5361869ad
Denis Fadeev <denis@fadeev.org>
2024-09-12 17:12:27 +0800

    Keep only universal app examples

---
+++ swap/hardhat.config.ts
@@ -0,0 +1,25 @@
+import "./tasks/deploy";
+import "@zetachain/localnet/tasks";
+import "@nomicfoundation/hardhat-toolbox";
+import "@zetachain/toolkit/tasks";
+
+import { getHardhatConfigNetworks } from "@zetachain/networks";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  networks: {
+    ...getHardhatConfigNetworks(),
+  },
+  solidity: {
+    compilers: [
+      { version: "0.5.10" /** For create2 factory */ },
+      { version: "0.6.6" /** For uniswap v2 router*/ },
+      { version: "0.5.16" /** For uniswap v2 core*/ },
+      { version: "0.4.19" /** For weth*/ },
+      { version: "0.8.7" },
+      { version: "0.8.26" },
+    ],
+  },
+};
+
+export default config;
