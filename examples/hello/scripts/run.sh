a750038a1478ecd559170076e022038c0239c620
Denis Fadeev <denis@fadeev.org>
2024-10-31 18:39:29 +0300

    unbreak

--- examples/hello/scripts/run.sh
+++ examples/hello/scripts/run.sh
@@ -12,7 +12,7 @@ npx hardhat echo-call \
   --contract 0x9E545E3C0baAB3E08CdfD552C960A1050f373042 \
   --receiver 0x84eA74d481Ee0A5332c457a4d796187F6Ba67fEB \
   --network localhost \
-  --types '["uint256"]' 42
+  --types '["string"]' alice
 
 npx hardhat localnet-check
 
