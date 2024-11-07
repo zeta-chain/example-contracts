b77396f0fc2bbf77c3064cd623af0fefcd6c81ac
Denis Fadeev <denis@fadeev.org>
2024-11-07 14:24:27 +0300

    Ownable2Step

--- examples/nft/contracts/Connected.sol
+++ examples/nft/contracts/Connected.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "@zetachain/protocol-contracts/contracts/evm/GatewayEVM.sol";
 import {RevertContext} from "@zetachain/protocol-contracts/contracts/Revert.sol";
 import "./shared/Events.sol";
@@ -14,7 +14,7 @@ contract Connected is
     ERC721,
     ERC721Enumerable,
     ERC721URIStorage,
-    Ownable,
+    Ownable2Step,
     Events
 {
     GatewayEVM public immutable gateway;
--- examples/nft/contracts/Universal.sol
+++ examples/nft/contracts/Universal.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.26;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {RevertContext, RevertOptions} from "@zetachain/protocol-contracts/contracts/Revert.sol";
 import "@zetachain/protocol-contracts/contracts/zevm/interfaces/UniversalContract.sol";
 import "@zetachain/protocol-contracts/contracts/zevm/interfaces/IGatewayZEVM.sol";
@@ -18,7 +18,7 @@ contract Universal is
     ERC721,
     ERC721Enumerable,
     ERC721URIStorage,
-    Ownable,
+    Ownable2Step,
     UniversalContract,
     Events
 {
