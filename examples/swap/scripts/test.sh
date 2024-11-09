50e721735a19319e42b804759211c49c35743b3b
Denis Fadeev <denis@fadeev.org>
2024-11-09 15:53:06 +0300

    swap

--- examples/swap/scripts/test.sh
+++ examples/swap/scripts/test.sh
@@ -23,10 +23,4 @@ npx hardhat swap-from-evm \
   --target "$ZRC20_USDC" \
   --recipient "$SENDER"
 
-npx hardhat localnet-check
-
-# npx hardhat swap-from-evm --network localhost --receiver 0x84eA74d481Ee0A5332c457a4d796187F6Ba67fEB --amount 1 --target 0x2ca7d64A7EFE2D62A725E2B35Cf7230D6677FfEe --recipient 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --erc20 0x0B306BF915C4d645ff596e518fAf3F9669b97016
-
-# npx hardhat localnet-stop
-
 if [ "$1" = "localnet" ]; then npx hardhat localnet-stop; fi
