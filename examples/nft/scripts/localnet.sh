02724709bd720ac0801a08239b88295667122881
Denis Fadeev <denis@fadeev.org>
2025-01-15 19:17:28 +0300

    wip

---
+++ examples/nft/scripts/localnet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sh ./node_modules/@zetachain/standard-contracts/contracts/nft/scripts/localnet.sh $1
---
+++ examples/token/scripts/localnet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sh ./node_modules/@zetachain/standard-contracts/contracts/token/scripts/localnet.sh $1
