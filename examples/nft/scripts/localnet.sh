846d7200a9aad0d4b9763ac0a543f5375e5299cb
Denis Fadeev <denis@fadeev.org>
2025-01-15 19:22:52 +0300

    use bash

--- examples/nft/scripts/localnet.sh
+++ examples/nft/scripts/localnet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sh ./node_modules/@zetachain/standard-contracts/contracts/nft/scripts/localnet.sh $1
+bash ./node_modules/@zetachain/standard-contracts/contracts/nft/scripts/localnet.sh $1
