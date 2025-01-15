d8732f75b1191b7948302016ef975224f0f90c0d
Denis Fadeev <denis@fadeev.org>
2025-01-15 19:09:50 +0300

    add nft and token examples

---
+++ examples/token/README.md
@@ -0,0 +1,3 @@
+# Universal NFT Example
+
+https://www.zetachain.com/docs/developers/standards/nft/
---
+++ examples/token/hardhat.config.ts
@@ -0,0 +1,32 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
+import * as dotenv from "dotenv";
+
+import "@zetachain/standard-contracts/tasks/token";
+import "@zetachain/localnet/tasks";
+import "@zetachain/toolkit/tasks";
+import { getHardhatConfig } from "@zetachain/toolkit/client";
+
+import "@nomiclabs/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  ...getHardhatConfig({ accounts: [process.env.PRIVATE_KEY] }),
+  solidity: {
+    compilers: [
+      {
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+        version: "0.8.26",
+      },
+    ],
+  },
+};
+
+export default config;
