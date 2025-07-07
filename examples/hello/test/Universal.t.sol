78d6c391795b076eb53a7c886aa8a2fed67825c4
Denis Fadeev <denis@fadeev.org>
2025-07-07 20:26:43 +0300

    hello: update toolkit

--- examples/hello/test/Universal.t.sol
+++ examples/hello/test/Universal.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.26;
 
-import "../../../FoundrySetup.t.sol";
+import "@zetachain/toolkit/contracts/testing/FoundrySetup.t.sol";
 import "../contracts/Universal.sol";
 import {RevertOptions} from "@zetachain/protocol-contracts/contracts/Revert.sol";
 
@@ -11,19 +11,19 @@ contract UniversalTest is FoundrySetup {
 
     function setUp() public override {
         super.setUp();
-        universal = new Universal(payable(address(zetaSetup.wrapGatewayZEVM())));
+        universal = new Universal(
+            payable(address(zetaSetup.wrapGatewayZEVM()))
+        );
         // console.log("Custody",  evmSetup.wrapGatewayEVM(5).custody());
         // console.log("Connector",  evmSetup.wrapGatewayEVM(5).zetaConnector());
         // console.log("Zeta token",  evmSetup.zetaToken(5));
         // console.log("TSS",  evmSetup.wrapGatewayEVM(5).tssAddress());
-        
-        
     }
 
     function test_crosschain_call_emitsHelloEvent() public {
         // Simulate call from EVM -> ZetaChain through wrapGatewayZEVM
         bytes memory encodedMessage = abi.encode(message);
-        
+
         RevertOptions memory revertOptions = RevertOptions({
             abortAddress: address(0),
             callOnRevert: false,
@@ -34,15 +34,13 @@ contract UniversalTest is FoundrySetup {
         // Listen for event on Universal
         vm.expectEmit(true, true, true, true);
         emit Universal.HelloEvent("Hello: ", message);
-        
+
         evmSetup.wrapGatewayEVM(5).call(
             address(universal),
             encodedMessage,
             revertOptions
         );
     }
-
-  
 }
 
 // Run test with Foundry:
