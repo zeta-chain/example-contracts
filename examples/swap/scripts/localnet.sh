23f01b173eea5969fe228da166347c36e95e886a
Denis Fadeev <denis@fadeev.org>
2025-07-11 10:11:17 +0300

    uncomment commands

--- examples/swap/scripts/localnet.sh
+++ examples/swap/scripts/localnet.sh
@@ -23,96 +23,95 @@ SENDER=0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 CONTRACT_SWAP=$(npx hardhat deploy --name Swap --network localhost --gateway "$GATEWAY_ZETACHAIN" --uniswap-router "$UNISWAP_ROUTER" | jq -r '.contractAddress')
 COMPANION=$(npx hardhat deploy-companion --gateway "$GATEWAY_ETHEREUM" --network localhost --json | jq -r '.contractAddress')
 
-# npx hardhat evm-swap \
-#   --network localhost \
-#   --receiver "$CONTRACT_SWAP" \
-#   --amount 0.1 \
-#   --target "$WZETA" \
-#   --gateway-evm "$GATEWAY_ETHEREUM" \
-#   --skip-checks \
-#   --withdraw false \
-#   --recipient "$SENDER"
+npx hardhat evm-swap \
+  --network localhost \
+  --receiver "$CONTRACT_SWAP" \
+  --amount 0.1 \
+  --target "$WZETA" \
+  --gateway-evm "$GATEWAY_ETHEREUM" \
+  --skip-checks \
+  --withdraw false \
+  --recipient "$SENDER"
 
-# yarn zetachain localnet check
+yarn zetachain localnet check
 
-# npx hardhat evm-swap \
-#   --network localhost \
-#   --receiver "$CONTRACT_SWAP" \
-#   --amount 0.1 \
-#   --gateway-evm "$GATEWAY_ETHEREUM" \
-#   --target "$ZRC20_USDC" \
-#   --skip-checks \
-#   --recipient "$SENDER"
+npx hardhat evm-swap \
+  --network localhost \
+  --receiver "$CONTRACT_SWAP" \
+  --amount 0.1 \
+  --gateway-evm "$GATEWAY_ETHEREUM" \
+  --target "$ZRC20_USDC" \
+  --skip-checks \
+  --recipient "$SENDER"
 
-# yarn zetachain localnet check
+yarn zetachain localnet check
 
-# npx hardhat evm-swap \
-#   --network localhost \
-#   --receiver "$CONTRACT_SWAP" \
-#   --amount 0.1 \
-#   --target "$ZRC20_BNB" \
-#   --gateway-evm "$GATEWAY_ETHEREUM" \
-#   --skip-checks \
-#   --erc20 "$USDC_ETHEREUM" \
-#   --recipient "$SENDER"
+npx hardhat evm-swap \
+  --network localhost \
+  --receiver "$CONTRACT_SWAP" \
+  --amount 0.1 \
+  --target "$ZRC20_BNB" \
+  --gateway-evm "$GATEWAY_ETHEREUM" \
+  --skip-checks \
+  --erc20 "$USDC_ETHEREUM" \
+  --recipient "$SENDER"
 
-# yarn zetachain localnet check
+yarn zetachain localnet check
 
-# npx hardhat evm-swap \
-#   --skip-checks \
-#   --network localhost \
-#   --receiver "$CONTRACT_SWAP" \
-#   --amount 0.1 \
-#   --gateway-evm "$GATEWAY_ETHEREUM" \
-#   --target "$ZRC20_BNB" \
-#   --recipient "$SENDER"
+npx hardhat evm-swap \
+  --skip-checks \
+  --network localhost \
+  --receiver "$CONTRACT_SWAP" \
+  --amount 0.1 \
+  --gateway-evm "$GATEWAY_ETHEREUM" \
+  --target "$ZRC20_BNB" \
+  --recipient "$SENDER"
 
-# yarn zetachain localnet check
+yarn zetachain localnet check
 
-# npx hardhat evm-swap \
-#   --skip-checks \
-#   --network localhost \
-#   --receiver "$CONTRACT_SWAP" \
-#   --amount 0.1 \
-#   --target "$ZRC20_BNB" \
-#   --gateway-evm "$GATEWAY_ETHEREUM" \
-#   --recipient "$SENDER" \
-#   --withdraw false
+npx hardhat evm-swap \
+  --skip-checks \
+  --network localhost \
+  --receiver "$CONTRACT_SWAP" \
+  --amount 0.1 \
+  --target "$ZRC20_BNB" \
+  --gateway-evm "$GATEWAY_ETHEREUM" \
+  --recipient "$SENDER" \
+  --withdraw false
 
-# yarn zetachain localnet check
+yarn zetachain localnet check
 
-# npx hardhat companion-swap \
-#   --network localhost \
-#   --skip-checks \
-#   --contract "$COMPANION" \
-#   --universal-contract "$CONTRACT_SWAP" \
-#   --amount 0.1 \
-#   --target "$ZRC20_BNB" \
-#   --recipient "$SENDER" 
+npx hardhat companion-swap \
+  --network localhost \
+  --skip-checks \
+  --contract "$COMPANION" \
+  --universal-contract "$CONTRACT_SWAP" \
+  --amount 0.1 \
+  --target "$ZRC20_BNB" \
+  --recipient "$SENDER" 
 
-# yarn zetachain localnet check
+yarn zetachain localnet check
 
-# npx hardhat companion-swap \
-#   --skip-checks \
-#   --network localhost \
-#   --contract "$COMPANION" \
-#   --universal-contract "$CONTRACT_SWAP" \
-#   --amount 0.1 \
-#   --erc20 "$USDC_ETHEREUM" \
-#   --target "$ZRC20_BNB" \
-#   --recipient "$SENDER"
+npx hardhat companion-swap \
+  --skip-checks \
+  --network localhost \
+  --contract "$COMPANION" \
+  --universal-contract "$CONTRACT_SWAP" \
+  --amount 0.1 \
+  --erc20 "$USDC_ETHEREUM" \
+  --target "$ZRC20_BNB" \
+  --recipient "$SENDER"
 
-# yarn zetachain localnet check
+yarn zetachain localnet check
 
-# npx hardhat zetachain-swap \
-#   --network localhost \
-#   --contract "$CONTRACT_SWAP" \
-#   --amount 0.1 \
-#   --zrc20 "$ZRC20_BNB" \
-#   --target "$ZRC20_ETHEREUM" \
-#   --recipient "$SENDER"
+npx hardhat zetachain-swap \
+  --network localhost \
+  --contract "$CONTRACT_SWAP" \
+  --amount 0.1 \
+  --zrc20 "$ZRC20_BNB" \
+  --target "$ZRC20_ETHEREUM" \
+  --recipient "$SENDER"
 
-# yarn zetachain localnet check
+yarn zetachain localnet check
 
-
-# yarn zetachain localnet stop
+yarn zetachain localnet stop
