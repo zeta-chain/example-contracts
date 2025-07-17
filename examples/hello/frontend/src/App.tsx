29933600383ae9768df9e2f3b82828e9a5cac9de
Hernan Clich <hernan.g.clich@gmail.com>
2025-07-17 16:24:37 -0300

    Reordered component

--- examples/hello/frontend/src/App.tsx
+++ examples/hello/frontend/src/App.tsx
@@ -60,27 +60,31 @@ function App() {
                   const signer =
                     (await ethersProvider.getSigner()) as ethers.AbstractSigner;
 
-                  const result = await evmCall(
-                    {
-                      receiver: '0x61a184EB30D29eD0395d1ADF38CC7d2F966c4A82',
-                      types: ['string'],
-                      values: ['hello'],
-                      revertOptions: {
-                        callOnRevert: false,
-                        revertAddress: ZeroAddress,
-                        revertMessage: '',
-                        abortAddress: ZeroAddress,
-                        onRevertGasLimit: 1000000,
-                      },
+                  const helloUniversalContractAddress =
+                    '0x61a184EB30D29eD0395d1ADF38CC7d2F966c4A82';
+
+                  const evmCallParams = {
+                    receiver: helloUniversalContractAddress,
+                    types: ['string'],
+                    values: ['hello'],
+                    revertOptions: {
+                      callOnRevert: false,
+                      revertAddress: ZeroAddress,
+                      revertMessage: '',
+                      abortAddress: ZeroAddress,
+                      onRevertGasLimit: 1000000,
                     },
-                    {
-                      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                      signer: signer as any,
-                      txOptions: {
-                        gasLimit: 1000000,
-                      },
-                    }
-                  );
+                  };
+
+                  const evmCallOptions = {
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    signer: signer as any,
+                    txOptions: {
+                      gasLimit: 1000000,
+                    },
+                  };
+
+                  const result = await evmCall(evmCallParams, evmCallOptions);
 
                   console.debug('result', result);
                 }}
