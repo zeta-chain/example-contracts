40df1fed00f2ca20c1a6ac364928a2c04f90b94c
Hernan Clich <hernan.g.clich@gmail.com>
2025-07-18 13:23:42 -0300

    Made header button look cool

--- examples/hello/frontend/src/components/Header.tsx
+++ examples/hello/frontend/src/components/Header.tsx
@@ -3,21 +3,34 @@ import './Header.css';
 import { useWallet } from '../hooks/useWallet';
 import { IconZetaChainLogo } from './IconZetaChainLogo';
 
+const truncateAddress = (address: string) => {
+  return `${address.slice(0, 6)}...${address.slice(-4)}`;
+};
+
 export const Header = () => {
-  const { isConnected, disconnectWallet } = useWallet();
+  const { account, disconnectWallet, selectedProvider } = useWallet();
 
   return (
     <div className="header-container">
       <IconZetaChainLogo className="header-logo" />
-      {isConnected && (
-        <button
-          className="header-disconnect-button"
-          type="button"
-          onClick={disconnectWallet}
-        >
-          <span className="sm-only">X</span>
-          <span className="lg-only">Disconnect</span>
-        </button>
+      {!!account && (
+        <div className="header-connected-container">
+          <div>
+            <img
+              src={selectedProvider?.info.icon}
+              alt={selectedProvider?.info.name}
+              className="header-provider-icon"
+            />
+            <span className="lg-only">{truncateAddress(account)}</span>
+          </div>
+          <button
+            className="header-disconnect-button"
+            type="button"
+            onClick={disconnectWallet}
+          >
+            <span>X</span>
+          </button>
+        </div>
       )}
     </div>
   );
