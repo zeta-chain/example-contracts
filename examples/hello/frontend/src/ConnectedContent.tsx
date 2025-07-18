2aea6e40d90af29328f6b6a88d1cfba8763bb1b5
Hernan Clich <hernan.g.clich@gmail.com>
2025-07-18 16:53:14 -0300

    Add input

--- examples/hello/frontend/src/ConnectedContent.tsx
+++ examples/hello/frontend/src/ConnectedContent.tsx
@@ -2,6 +2,7 @@ import './ConnectedContent.css';
 
 import { evmCall } from '@zetachain/toolkit/chains/evm';
 import { ethers, ZeroAddress } from 'ethers';
+import { useState } from 'react';
 
 import type { SupportedChain } from './constants/chains';
 import type { EIP6963ProviderDetail } from './types/wallet';
@@ -15,6 +16,44 @@ export function ConnectedContent({
   selectedProvider,
   supportedChain,
 }: ConnectedContentProps) {
+  const MAX_STRING_LENGTH = 20;
+  const [stringValue, setStringValue] = useState('');
+
+  const handleEvmCall = async () => {
+    const ethersProvider = new ethers.BrowserProvider(
+      selectedProvider.provider
+    );
+    const signer = (await ethersProvider.getSigner()) as ethers.AbstractSigner;
+
+    const helloUniversalContractAddress =
+      '0x61a184EB30D29eD0395d1ADF38CC7d2F966c4A82';
+
+    const evmCallParams = {
+      receiver: helloUniversalContractAddress,
+      types: ['string'],
+      values: [stringValue],
+      revertOptions: {
+        callOnRevert: false,
+        revertAddress: ZeroAddress,
+        revertMessage: '',
+        abortAddress: ZeroAddress,
+        onRevertGasLimit: 1000000,
+      },
+    };
+
+    const evmCallOptions = {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      signer: signer as any,
+      txOptions: {
+        gasLimit: 1000000,
+      },
+    };
+
+    const result = await evmCall(evmCallParams, evmCallOptions);
+
+    console.debug('result', result);
+  };
+
   return (
     <div className="main-container">
       <h1>Ready to say "Hello" on: {supportedChain.name}</h1>
@@ -22,47 +61,28 @@ export function ConnectedContent({
         This transaction will emit a cross-chain "HelloEvent" event on ZetaChain
         testnet's Universal Hello contract.
       </p>
-      <button
-        type="button"
-        className="call-button"
-        onClick={async () => {
-          const ethersProvider = new ethers.BrowserProvider(
-            selectedProvider.provider
-          );
-          const signer =
-            (await ethersProvider.getSigner()) as ethers.AbstractSigner;
-
-          const helloUniversalContractAddress =
-            '0x61a184EB30D29eD0395d1ADF38CC7d2F966c4A82';
-
-          const evmCallParams = {
-            receiver: helloUniversalContractAddress,
-            types: ['string'],
-            values: ['hello'],
-            revertOptions: {
-              callOnRevert: false,
-              revertAddress: ZeroAddress,
-              revertMessage: '',
-              abortAddress: ZeroAddress,
-              onRevertGasLimit: 1000000,
-            },
-          };
-
-          const evmCallOptions = {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            signer: signer as any,
-            txOptions: {
-              gasLimit: 1000000,
-            },
-          };
-
-          const result = await evmCall(evmCallParams, evmCallOptions);
-
-          console.debug('result', result);
-        }}
-      >
-        Evm Call 🚀
-      </button>
+      <div className="input-container">
+        <div className="input-container-inner">
+          <input
+            name="message-input"
+            className="call-input"
+            type="text"
+            placeholder="Enter your message"
+            value={stringValue}
+            onChange={(e) => {
+              if (e.target.value.length <= MAX_STRING_LENGTH) {
+                setStringValue(e.target.value);
+              }
+            }}
+          />
+          <button type="button" className="call-button" onClick={handleEvmCall}>
+            Evm Call 🚀
+          </button>
+        </div>
+        <span className="input-counter">
+          {stringValue.length} / {MAX_STRING_LENGTH} characters
+        </span>
+      </div>
     </div>
   );
 }
