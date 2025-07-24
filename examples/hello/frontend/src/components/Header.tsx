aaa943be25d6df773b1b9918b0dcc81a4f18b3bf
Hernan Clich <hernan.g.clich@gmail.com>
2025-07-24 11:15:29 -0300

    Removed chain from header

--- examples/hello/frontend/src/components/Header.tsx
+++ examples/hello/frontend/src/components/Header.tsx
@@ -1,18 +1,12 @@
 import './Header.css';
 
-import { SUPPORTED_CHAINS } from '../constants/chains';
 import { useWallet } from '../hooks/useWallet';
 import { truncateAddress } from '../utils/truncate';
 import { ConnectWallet } from './ConnectWallet';
 import { ThemeToggle } from './ThemeToggle';
 
 export const Header = () => {
-  const { account, disconnectWallet, decimalChainId, selectedProvider } =
-    useWallet();
-
-  const supportedChain = SUPPORTED_CHAINS.find(
-    (chain) => chain.chainId === decimalChainId
-  );
+  const { account, disconnectWallet, selectedProvider } = useWallet();
 
   return (
     <div className="header-container">
@@ -24,16 +18,6 @@ export const Header = () => {
         ) : (
           <div className="header-connected-container">
             <div>
-              <div className="header-chain-icon-container">
-                <img
-                  src={
-                    supportedChain?.icon ||
-                    '/logos/network-placeholder-logo.svg'
-                  }
-                  alt={supportedChain?.name || 'Unsupported network'}
-                  className="header-chain-icon"
-                />
-              </div>
               <div className="header-provider-container">
                 <img
                   src={selectedProvider?.info.icon}
