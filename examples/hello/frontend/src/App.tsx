e9232c8f8d9d30290ec568b37a551284c1af1765
Hernan Clich <hernan.g.clich@gmail.com>
2025-08-26 15:41:29 -0300

    Using UniversalSignInContextProvider

--- examples/hello/frontend/src/App.tsx
+++ examples/hello/frontend/src/App.tsx
@@ -1,34 +1,17 @@
-import '@zetachain/wallet/ethereum';
-
-import { EthereumWalletConnectors } from '@dynamic-labs/ethereum';
-import {
-  DynamicContextProvider,
-  DynamicUserProfile,
-} from '@dynamic-labs/sdk-react-core';
+import { UniversalSignInContextProvider } from '@zetachain/wallet/react';
 
 import { AppContent } from './AppContent';
 import { Header } from './components/Header';
-import { evmNetworks } from './constants/chains';
 import { useTheme } from './hooks/useTheme';
 
 function App() {
   const { theme } = useTheme();
 
   return (
-    <DynamicContextProvider
-      settings={{
-        environmentId: 'eaec6949-d524-40e7-81d2-80113243499a',
-        walletConnectors: [EthereumWalletConnectors],
-        overrides: {
-          evmNetworks,
-        },
-      }}
-      theme={theme}
-    >
+    <UniversalSignInContextProvider environment="sandbox" theme={theme}>
       <Header />
       <AppContent />
-      <DynamicUserProfile />
-    </DynamicContextProvider>
+    </UniversalSignInContextProvider>
   );
 }
 
