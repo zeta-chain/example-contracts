b97cfbc32c25cdf4a9a4aff640960c02f16512e0
Hernan Clich <hernan.g.clich@gmail.com>
2025-08-26 10:35:55 -0300

    Opening USI GWallet directly

--- examples/hello/frontend/src/components/ConnectDynamicWallet.tsx
+++ examples/hello/frontend/src/components/ConnectDynamicWallet.tsx
@@ -1,24 +1,29 @@
 import './ConnectDynamicWallet.css';
 
 import {
-  DynamicConnectButton,
   DynamicWidget,
   useDynamicContext,
+  useWalletOptions,
 } from '@dynamic-labs/sdk-react-core';
 
+import { Button } from './Button';
 import { IconWallet } from './icons';
 
 export const ConnectDynamicWallet = () => {
   const { primaryWallet } = useDynamicContext();
+  const { selectWalletOption } = useWalletOptions();
 
   if (!primaryWallet?.address) {
     return (
-      <DynamicConnectButton buttonClassName="dynamic-connect-button">
+      <Button
+        className="dynamic-connect-button"
+        onClick={() => selectWalletOption('universalsigninevm')}
+      >
         <div className="dynamic-connect-button-content">
           <IconWallet />
           Connect Wallet
         </div>
-      </DynamicConnectButton>
+      </Button>
     );
   }
 
