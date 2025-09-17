2452439472958ec27f5ed463abed76d82cd207b8
Hernan Clich <hernan.g.clich@gmail.com>
2025-09-17 17:29:17 -0300

    Optimize provider wrapping

--- examples/hello/frontend/src/App.tsx
+++ examples/hello/frontend/src/App.tsx
@@ -2,16 +2,22 @@ import { UniversalSignInContextProvider } from '@zetachain/wallet/react';
 
 import { AppContent } from './AppContent';
 import { Header } from './components/Header';
+import { USE_DYNAMIC_WALLET } from './constants/wallets';
 import { useTheme } from './hooks/useTheme';
 
 function App() {
   const { theme } = useTheme();
 
-  return (
+  return USE_DYNAMIC_WALLET ? (
     <UniversalSignInContextProvider environment="sandbox" theme={theme}>
       <Header />
       <AppContent />
     </UniversalSignInContextProvider>
+  ) : (
+    <>
+      <Header />
+      <AppContent />
+    </>
   );
 }
 
--- examples/hello/frontend/src/main.tsx
+++ examples/hello/frontend/src/main.tsx
@@ -5,15 +5,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
+import { USE_DYNAMIC_WALLET } from './constants/wallets.ts';
 import { Eip6963WalletProvider } from './context/Eip6963WalletProvider.tsx';
 import { ThemeProvider } from './context/ThemeProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <Eip6963WalletProvider>
+      {USE_DYNAMIC_WALLET ? (
         <App />
-      </Eip6963WalletProvider>
+      ) : (
+        <Eip6963WalletProvider>
+          <App />
+        </Eip6963WalletProvider>
+      )}
     </ThemeProvider>
   </StrictMode>
 );
