eb95949845f2d6db8a7aaac6f8ae4e77683f7fd9
Hernan Clich <hernan.g.clich@gmail.com>
2025-09-17 17:00:40 -0300

    Updated dynamic version header

--- examples/hello/frontend/src/components/Header.tsx
+++ examples/hello/frontend/src/components/Header.tsx
@@ -1,40 +1,54 @@
 import './Header.css';
 
+import { useUniversalSignInContext } from '@zetachain/wallet/react';
+
 import { USE_DYNAMIC_WALLET } from '../constants/wallets';
 import { useEip6963Wallet } from '../hooks/useEip6963Wallet';
 import { ConnectDynamicWallet } from './ConnectDynamicWallet';
 import { ConnectEip6963Wallet } from './ConnectEip6963Wallet';
+import { DynamicWalletControls } from './DynamicWalletControls';
 import { ThemeToggle } from './ThemeToggle';
 import { WalletControls } from './WalletControls';
 
-export const Header = () => {
+const Eip6963Header = () => {
   const { isConnected } = useEip6963Wallet();
-
-  if (!USE_DYNAMIC_WALLET) {
-    return (
-      <div className="header-container">
-        <div className="header-controls">
-          {!isConnected ? (
-            <div className="lg-only">
-              <ConnectEip6963Wallet />
-            </div>
-          ) : (
-            <WalletControls />
-          )}
-          <ThemeToggle />
-        </div>
-      </div>
-    );
-  }
 
   return (
     <div className="header-container">
       <div className="header-controls">
-        <div>
-          <ConnectDynamicWallet />
-        </div>
+        {!isConnected ? (
+          <div className="lg-only">
+            <ConnectEip6963Wallet />
+          </div>
+        ) : (
+          <WalletControls />
+        )}
         <ThemeToggle />
       </div>
     </div>
   );
+};
+
+const DynamicHeader = () => {
+  const { primaryWallet } = useUniversalSignInContext();
+  const isDynamicConnected = !!primaryWallet?.address;
+
+  return (
+    <div className="header-container">
+      <div className="header-controls">
+        {!isDynamicConnected ? (
+          <div className="lg-only">
+            <ConnectDynamicWallet />
+          </div>
+        ) : (
+          <DynamicWalletControls />
+        )}
+        <ThemeToggle />
+      </div>
+    </div>
+  );
+};
+
+export const Header = () => {
+  return USE_DYNAMIC_WALLET ? <DynamicHeader /> : <Eip6963Header />;
 };
