333fb98a3d91438555eb60b08e7910cd04602cd9
Denis Fadeev <denis@fadeev.org>
2025-10-21 15:31:03 +0300

    remove import of tasks

--- examples/nft/hardhat.config.ts
+++ examples/nft/hardhat.config.ts
@@ -2,8 +2,6 @@ import "@nomicfoundation/hardhat-toolbox";
 import { HardhatUserConfig } from "hardhat/config";
 import * as dotenv from "dotenv";
 
-import "@zetachain/localnet/tasks";
-import "@zetachain/toolkit/tasks";
 import { getHardhatConfig } from "@zetachain/toolkit/utils";
 
 import "@nomiclabs/hardhat-ethers";
